6f92cb53b1d8c213bb1044e437bacc2c16d7d3e9
2017030473 <hgs3896@hanyang.ac.kr>
2022-04-17 01:40:16 +0900

    Add set_cpu_share syscall tests

--- xv6-public/stridetests.c
+++ xv6-public/stridetests.c
@@ -2,16 +2,26 @@
 #include "stat.h"
 #include "user.h"
 
-#define TESTCASE(N, cond) \
-    printf(1, "Testcase #" #N ": "); \
-    printf(1, (cond) ? "correct\n" : "fail\n");
+#define TESTCASE(N, cond)                                                      \
+  printf(1, "Testcase #" #N ": ");                                             \
+  printf(1, (cond) ? "correct\n" : "fail\n");
 
 int
 main(int argc, char *argv[])
 {
-    TESTCASE(1, set_cpu_share(100) == -1);
-    TESTCASE(2, set_cpu_share(50) == 0);
-    TESTCASE(3, set_cpu_share(0) == -1);
-    TESTCASE(4, set_cpu_share(-10) == -1);
-    exit();
+  uint x;
+  TESTCASE(1, set_cpu_share(100) == -1);
+  TESTCASE(2, set_cpu_share(0) == -1);
+  TESTCASE(3, set_cpu_share(-10) == -1);
+  TESTCASE(4, set_cpu_share(50) == 0);
+  x = 0; while(x++ < 4000000000);
+  printf(1, "Finished\n");
+  TESTCASE(5, set_cpu_share(70) == 0);
+  x = 0; while(x++ < 4000000000);
+  printf(1, "Finished\n");
+  TESTCASE(6, set_cpu_share(80) == 0);
+  x = 0; while(x++ < 4000000000);
+  printf(1, "Finished\n");
+  TESTCASE(6, set_cpu_share(81) == -1);
+  exit();
 }
