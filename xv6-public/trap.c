7bf66ef317d66c283ec1d7728c0d71e93774cf3e
2017030473 <hgs3896@hanyang.ac.kr>
2022-05-21 22:49:30 +0900

    Add a tid number in the trap error msg

--- xv6-public/trap.c
+++ xv6-public/trap.c
@@ -89,9 +89,9 @@ trap(struct trapframe *tf)
       panic("trap");
     }
     // In user space, assume process misbehaved.
-    cprintf("pid %d %s: trap %d err %d on cpu %d "
+    cprintf("pid %d tid %d %s: trap %d err %d on cpu %d "
             "eip 0x%x addr 0x%x--kill proc\n",
-            myproc()->pid, myproc()->name, tf->trapno,
+            myproc()->pid, mylwp(myproc())->tid, myproc()->name, tf->trapno,
             tf->err, cpuid(), tf->eip, rcr2());
     myproc()->killed = 1;
   }
