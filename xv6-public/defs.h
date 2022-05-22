3ca5a4263abb2d0be1326b2b1d321aecb644ee3f
2017030473 <hgs3896@hanyang.ac.kr>
2022-05-22 16:16:17 +0900

    Add file name and line number to logging library

--- xv6-public/defs.h
+++ xv6-public/defs.h
@@ -212,32 +212,35 @@ int             set_cpu_share(int share);
 #define LOGGING            0
 #define LOG_LEVEL          DEBUG_LEVEL_ERROR
 
+#define TO_STR2(arg) #arg
+#define TO_STR(arg) TO_STR2(arg)
+
 #if LOGGING && LOG_LEVEL <= DEBUG_LEVEL_TRACE
-#define kprintf_trace(...) cprintf("[TRACE] " __VA_ARGS__)
+#define kprintf_trace(...) cprintf(__FILE__ ":" TO_STR(__LINE__) " [TRACE] " __VA_ARGS__)
 #else
 #define kprintf_trace(...)
 #endif
 
 #if LOGGING && LOG_LEVEL <= DEBUG_LEVEL_DEBUG
-#define kprintf_debug(...) cprintf("[DEBUG] " __VA_ARGS__)
+#define kprintf_debug(...) cprintf(__FILE__ ":" TO_STR(__LINE__) " [DEBUG] " __VA_ARGS__)
 #else
 #define kprintf_debug(...)
 #endif
 
 #if LOGGING && LOG_LEVEL <= DEBUG_LEVEL_INFO
-#define kprintf_info(...) cprintf("[INFO] " __VA_ARGS__)
+#define kprintf_info(...) cprintf(__FILE__ ":" TO_STR(__LINE__) " [INFO] " __VA_ARGS__)
 #else
 #define kprintf_info(...)
 #endif
 
 #if LOGGING && LOG_LEVEL <= DEBUG_LEVEL_WARN
-#define kprintf_warn(...) cprintf("[WARN] " __VA_ARGS__)
+#define kprintf_warn(...) cprintf(__FILE__ ":" TO_STR(__LINE__) " [WARN] " __VA_ARGS__)
 #else
 #define kprintf_warn(...)
 #endif
 
 #if LOGGING && LOG_LEVEL <= DEBUG_LEVEL_ERROR
-#define kprintf_error(...) cprintf("[ERROR] " __VA_ARGS__)
+#define kprintf_error(...) cprintf(__FILE__ ":" TO_STR(__LINE__) " [ERROR] " __VA_ARGS__)
 #else
 #define kprintf_error(...)
 #endif
