887d1b5107f98f09fd268e13d2d4eaca6d0b8d0b
2017030473 <hgs3896@hanyang.ac.kr>
2022-06-03 14:42:32 +0900

    Implement xv6 Readers-writer lock

--- xv6-public/rwlock.c
+++ xv6-public/rwlock.c
@@ -10,29 +10,65 @@
 #include "spinlock.h"
 #include "rwlock.h"
 
+extern void xem_init(xem_t *semaphore, int cap);
+extern void xem_wait(xem_t *semaphore);
+extern void xem_unlock(xem_t *semaphore);
+
 void
 rwlock_init(rwlock_t *rwlock)
 {
+  rwlock->num_readers = 0;
+  rwlock->num_writers = 0;
+  initlock(&rwlock->lk, "rwlock");
+  xem_init(&rwlock->write, 1);
 }
 
 void
 rwlock_acquire_readlock(rwlock_t *rwlock)
 {
+  int num_readers;
+
+  acquire(&rwlock->lk);
+  while(rwlock->num_writers > 0)
+    sleep(rwlock, &rwlock->lk);
+  num_readers = ++rwlock->num_readers;
+  if(num_readers == 1)
+    xem_wait(&rwlock->write);
+  release(&rwlock->lk);
 }
 
 void
 rwlock_acquire_writelock(rwlock_t *rwlock)
 {
+  acquire(&rwlock->lk);
+  rwlock->num_writers++;
+  release(&rwlock->lk);
+
+  xem_wait(&rwlock->write);
 }
 
 void
 rwlock_release_readlock(rwlock_t *rwlock)
 {
+  int num_readers;
+
+  acquire(&rwlock->lk);
+  num_readers = --rwlock->num_readers;
+  if(num_readers == 0)
+    xem_unlock(&rwlock->write);
+  release(&rwlock->lk);
 }
 
 void
 rwlock_release_writelock(rwlock_t *rwlock)
 {
+  xem_unlock(&rwlock->write);
+
+  acquire(&rwlock->lk);
+  rwlock->num_writers--;
+  release(&rwlock->lk);
+
+  wakeup(rwlock);
 }
 
 int
