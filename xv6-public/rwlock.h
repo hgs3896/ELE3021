11bdcf86fd8f77648c97439376a0f5cea9bd89e0
2017030473 <hgs3896@hanyang.ac.kr>
2022-06-05 16:04:36 +0900

    Fix incorrect comment on semaphore

--- xv6-public/rwlock.h
+++ xv6-public/rwlock.h
@@ -6,5 +6,5 @@ struct rwlock_t {
   struct spinlock lk; // Spinlock for protecting rwlock_t
   struct xem_t write; // Semaphore for writer lock
   uint num_readers;   // The number of readers
-  uint num_writers;   // The number of readers
+  uint num_writers;   // The number of writers
 };
