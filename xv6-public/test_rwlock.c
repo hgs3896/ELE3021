cc4b11eba4e783a2ea3c1b0dc59e5b86350f45cd
2017030473 <hgs3896@hanyang.ac.kr>
2022-06-05 16:25:59 +0900

    Supplement RW lock testcase

--- xv6-public/test_rwlock.c
+++ xv6-public/test_rwlock.c
@@ -2,12 +2,14 @@
 #include "stat.h"
 #include "user.h"
 
-xem_t print_sem;
+xem_t sem;
 rwlock_t rwlock;
+int readerElapsedTicks;
+int writerElapsedTicks;
 
 #define REP 50
-#define NTHREADS 63
-#define READER_WRITER_RATIO 0.99
+#define NTHREADS 10
+#define READERS_RATIO 0.9
 
 void * reader_with_rwlock(void *arg);
 void * writer_with_rwlock(void *arg);
@@ -16,20 +18,33 @@ void * writer2_with_sem(void *arg);
 void * reader2_with_rwlock(void *arg);
 void * writer2_with_rwlock(void *arg);
 
+void test1(void);
+void test2(void);
+
 int
 main(int argc, char *argv[])
 {
-  void *ret;
-  int start_tick;
-
-  thread_t t[NTHREADS];
-
-  xem_init(&print_sem);
+  xem_init(&sem);
   rwlock_init(&rwlock);
+
+  /* TEST for checking lock acquisition tendency */
+  test1();
+
+  /* TEST for efficiency of reader performance of RW lock */
+  test2();
+
+  exit();
+}
+
+void
+test1(void)
+{
+  thread_t t[NTHREADS];
+  void *ret;
 
   printf(1, "1. Logging the lock acquisition of a RW lock\n");
   for(int i = 0; i < NTHREADS; ++i) {
-    void* (*start_routine)(void *) = i >= READER_WRITER_RATIO * NTHREADS ? writer_with_rwlock : reader_with_rwlock;
+    void* (*start_routine)(void *) = i >= READERS_RATIO * NTHREADS ? writer_with_rwlock : reader_with_rwlock;
     if(thread_create(&t[i], start_routine, (void *)(i)) < 0) {
       printf(1, "panic at thread create\n");
       exit();
@@ -41,13 +56,23 @@ main(int argc, char *argv[])
       exit();
     }
   }
+}
 
-  int result[2];
+void
+test2(void)
+{
+  thread_t t[NTHREADS];
+  void *ret;
+  int readerResults[2];
+  int writerResults[2];
+
   printf(1, "2. Lock Efficiency Test\n");
   printf(1, "\ta. Using a simple binary semaphore\n");
-  start_tick = uptime();
+
+  readerElapsedTicks = 0;
+  writerElapsedTicks = 0;
   for(int i = 0; i < NTHREADS; ++i) {
-    void* (*start_routine)(void *) = i >= READER_WRITER_RATIO * NTHREADS ? writer2_with_sem : reader2_with_sem;
+    void* (*start_routine)(void *) = i >= READERS_RATIO * NTHREADS ? writer2_with_sem : reader2_with_sem;
     if(thread_create(&t[i], start_routine, (void *)(i)) < 0) {
       printf(1, "panic at thread create\n");
       exit();
@@ -59,12 +84,14 @@ main(int argc, char *argv[])
       exit();
     }
   }
-  result[0] = uptime() - start_tick;
+  readerResults[0] = readerElapsedTicks;
+  writerResults[0] = writerElapsedTicks;
   
   printf(1, "\tb. Using a readers-writer lock\n");
-  start_tick = uptime();
+  readerElapsedTicks = 0;
+  writerElapsedTicks = 0;
   for(int i = 0; i < NTHREADS; ++i) {
-    void* (*start_routine)(void *) = i >= READER_WRITER_RATIO * NTHREADS ? writer2_with_rwlock : reader2_with_rwlock;
+    void* (*start_routine)(void *) = i >= READERS_RATIO * NTHREADS ? writer2_with_rwlock : reader2_with_rwlock;
     if(thread_create(&t[i], start_routine, (void *)(i)) < 0) {
       printf(1, "panic at thread create\n");
       exit();
@@ -76,9 +103,11 @@ main(int argc, char *argv[])
       exit();
     }
   }
-  result[1] = uptime() - start_tick;
-  printf(1, "2-a) %d ticks, 2-b) %d ticks\n", result[0], result[1]);
-  exit();
+  readerResults[1] = readerElapsedTicks;
+  writerResults[1] = writerElapsedTicks;
+
+  printf(1, "\n\tReader Elapsed Ticks 2-a) %d ticks\t2-b) %d ticks\n", readerResults[0], readerResults[1]);
+  printf(1, "\tWriter Elapsed Ticks 2-a) %d ticks\t2-b) %d ticks\n", writerResults[0], writerResults[1]);
 }
 
 void *
@@ -88,11 +117,12 @@ reader_with_rwlock(void *arg)
 
   for(int rep = 0; rep < REP; ++rep) {
     rwlock_acquire_readlock(&rwlock);
-    xem_wait(&print_sem);
+    xem_wait(&sem);
     printf(1, "Reader Accquired %d\n", id);
-    xem_unlock(&print_sem);
+    xem_unlock(&sem);
     rwlock_release_readlock(&rwlock);
   }
+
   thread_exit(0);
   return 0;
 }
@@ -105,16 +135,16 @@ writer_with_rwlock(void *arg)
   for(int rep = 0; rep < REP; ++rep) {
     rwlock_acquire_writelock(&rwlock);
     
-    xem_wait(&print_sem);
+    xem_wait(&sem);
     printf(1, "Writer Accquired %d\n", id);
-    xem_unlock(&print_sem);
+    xem_unlock(&sem);
 
     volatile int n = 0;
     while(++n < 10000000); // Do something
 
-    xem_wait(&print_sem);
+    xem_wait(&sem);
     printf(1, "Writer Released %d\n", id);
-    xem_unlock(&print_sem);
+    xem_unlock(&sem);
 
     rwlock_release_writelock(&rwlock);
   }
@@ -123,8 +153,7 @@ writer_with_rwlock(void *arg)
   return 0;
 }
 
-volatile
-int data[10000];
+volatile int data[10000];
 
 void *
 reader2_with_sem(void *arg)
@@ -132,13 +161,16 @@ reader2_with_sem(void *arg)
   int sum;
 
   for(int rep = 0; rep < REP; ++rep) {
-    sum = 0;  
-    xem_wait(&print_sem);
+    sum = 0;
+    int startTick = uptime();
+    xem_wait(&sem);
     for(int i = 0; i < 10000; ++i)
       sum += data[i];
     if(!(sum == 0 || sum == 5000 * 10001))
-      printf(1, "bad\n");
-    xem_unlock(&print_sem);
+      printf(1, "Race detected\n");
+    xem_unlock(&sem);
+    int elapsedTick = uptime() - startTick;
+    __sync_add_and_fetch(&readerElapsedTicks, elapsedTick);
   }
 
   thread_exit(0);
@@ -151,17 +183,20 @@ writer2_with_sem(void *arg)
   int id = (int)arg;
 
   for(int rep = 0; rep < REP; ++rep) {
+    int startTick = uptime();
     if(id % 2 == 0){
-      xem_wait(&print_sem);
+      xem_wait(&sem);
       for(int i = 0; i < 10000; ++i)
         data[i] = i + 1;
-      xem_unlock(&print_sem);
+      xem_unlock(&sem);
     }else{
-      xem_wait(&print_sem);
+      xem_wait(&sem);
       for(int i = 0; i < 10000; ++i)
         data[i] = 10000 - i;
-      xem_unlock(&print_sem);
+      xem_unlock(&sem);
     }
+    int elapsedTick = uptime() - startTick;
+    __sync_add_and_fetch(&writerElapsedTicks, elapsedTick);
   }
 
   thread_exit(0);
@@ -174,13 +209,16 @@ reader2_with_rwlock(void *arg)
   int sum;
 
   for(int rep = 0; rep < REP; ++rep) {
-    sum = 0;  
+    sum = 0;
+    int startTick = uptime();
     rwlock_acquire_readlock(&rwlock);
     for(int i = 0; i < 10000; ++i)
       sum += data[i];
     if(!(sum == 0 || sum == 5000 * 10001))
-      printf(1, "bad\n");
+      printf(1, "Race detected\n");
     rwlock_release_readlock(&rwlock);
+    int elapsedTick = uptime() - startTick;
+    __sync_add_and_fetch(&readerElapsedTicks, elapsedTick);
   }
 
   thread_exit(0);
@@ -193,6 +231,7 @@ writer2_with_rwlock(void *arg)
   int id = (int)arg;
 
   for(int rep = 0; rep < REP; ++rep) {
+    int startTick = uptime();
     if(id % 2 == 0){
       rwlock_acquire_writelock(&rwlock);
       for(int i = 0; i < 10000; ++i)
@@ -204,6 +243,8 @@ writer2_with_rwlock(void *arg)
         data[i] = 10000 - i;
       rwlock_release_writelock(&rwlock);
     }
+    int elapsedTick = uptime() - startTick;
+    __sync_add_and_fetch(&writerElapsedTicks, elapsedTick);
   }
 
   thread_exit(0);
